6c4d1445c5b0994cbd06a753dd666c2f713a8b83
Kyrushiqi <Karinalam39@gmail.com>
2025-04-07 01:55:38 -0400

    49/52 testcases passed

--- src/E_242_Valid_Anagram.java
+++ src/E_242_Valid_Anagram.java
@@ -1,17 +1,47 @@
 /*
-First Logic: Use a hashmap to store key value pairs (String & int) and use it to map out string s. Then make t into a hashmap and compare the two hashmaps.
+Question: https://leetcode.com/problems/valid-anagram/
+First Logic: Use a hashmap to store key value pairs (String & int) and use it to map out string s.
+Then make t into a hashmap and compare the two hashmaps.
+
 */
 import java.util.HashMap;
 
 class E_242_Valid_Anagram {
     public boolean isAnagram(String s, String t) {
-        HashMap sMap = new HashMap(s.length());
-        HashMap tMap = new HashMap(s.length());
+        HashMap<Character, Integer> sMap = new HashMap<>(s.length());
+        HashMap<Character, Integer> tMap = new HashMap<>(s.length());
+        boolean result = false;
 
         if(s.length() != t.length()){
             return false;
         }
 
-        return true;
+        for(int i = 0; i < s.length(); i++){
+            if(sMap.containsKey(s.charAt(i))){
+                sMap.replace(s.charAt(i), sMap.get(s.charAt(i))+1);
+            } else {
+                sMap.put(s.charAt(i), 1);
+            }
+        }
+
+        for(int i = 0; i < t.length(); i++){
+            if(tMap.containsKey(t.charAt(i))){
+                tMap.replace(t.charAt(i), tMap.get(t.charAt(i))+1);
+            } else {
+                tMap.put(t.charAt(i), 1);
+            }
+        }
+
+        for(int j = 0; j < sMap.size(); j++){
+            if(!(sMap.containsKey(s.charAt(j)) && tMap.containsKey(s.charAt(j)))){
+                return false;
+            } else {
+                if(sMap.get(s.charAt(j)).equals(tMap.get(s.charAt(j)))){
+                    result = true;
+                }
+            }
+        }
+
+        return result;
     }
 }
